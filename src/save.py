fe2edeab640b8725867cd3ba7995e831e967b63a
Bertha Soliany Frandi <163696570+BerthaSoliany@users.noreply.github.com>
2024-05-09 23:29:19 +0700

    feat: add save function

---
+++ src/save.py
@@ -0,0 +1,49 @@
+# PROGRAM F15 - Save
+# Writer: Bertha Soliany Frandi
+# Tanggal: 9 Mei 2024
+
+# KAMUS
+
+# ALGORITMA
+import os
+import time # untuk time.sleep
+# setiap function akan return array yang sudah dimodifikasi (karena hasil permainan)
+
+def save():
+    folder = input("Masukkan nama folder: ")
+    print()
+    print("Saving...")
+    time.sleep(1.5)
+    print()
+
+    # pengecekan keberadaan folder parent ./data
+    parent_check = os.path.exists("C:/Data lain//BERTHA/Python/TUBES/data")
+    if parent_check == False:
+        os.makedirs("C:/Data lain//BERTHA/Python/TUBES/data")
+        print("Membuat folder data...")
+        time.sleep(1.5)
+        
+    # pengecekan nama folder pada folder parent ./data
+    folder_check = os.path.exists(f"C:/Data lain//BERTHA/Python/TUBES/data/{folder}")
+    if folder_check == False:
+        os.makedirs(f"./data/{folder}")
+        print(f"Membuat folder data/{folder}")
+        time.sleep(2)
+
+    # overwrite data
+    write_csv(f"./data/{folder}/user.csv", array_csv_user)
+    write_csv(f"./data/{folder}/monster.csv", array_csv_monster)
+    write_csv(f"./data/{folder}/item_inventory.csv", array_csv_item_inventory)
+    write_csv(f"./data/{folder}/monster_inventory.csv", array_csv_monster_inventory)
+    write_csv(f"./data/{folder}/item_shop.csv", array_csv_item_shop)
+    write_csv(f"./data/{folder}/monster_shop.csv", array_csv_monster_shop)
+
+    print(f"Berhasil menyimpan data di folder data/{folder}!")
+
+# untuk overwrite data
+def write_csv(path: str, data: list):
+    with open(path, "w") as file: 
+        for row in data:
+            file.write(';'.join(map(str, row)) + '\n')
+
+save()
