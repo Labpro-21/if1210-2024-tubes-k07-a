c0a0a35bec318d357f42506e3fe47d917cf180e5
Naomi Risaka Sitorus <163662184+naomirisaka@users.noreply.github.com>
2024-05-10 15:10:18 +0700

    refactor: change var name for arrays used

--- src/laboratory.py
+++ src/laboratory.py
@@ -3,28 +3,18 @@
 # Tanggal: 5 Mei 2024
 
 # KAMUS
-# monster_amt: integer
-# coin_amt : integer 
-# price : integer
-# monster_id : integer
-# upgrade_id : integer 
-# level: integer
+# monster_amt, coin_amt, price, monster_id, upgrade_id, level: integer
 # confirm: string
 
 # ALGORITMA
-import login
-from monster import csv_to_array
-
-def laboratory(array, user_id):
+def laboratory(array_user, array_monster_inventory, user_id):
     print('Kamu telah memasuki area lab monster!')
     print('---------- MONSTERS OWNED ----------')
-    inventory_array = csv_to_array (r"E:\NAOMI\University\Sem 2\Programming Fundamentals\Tubes\monster_inventory.csv")
-    monster_array = csv_to_array (r"E:\NAOMI\University\Sem 2\Programming Fundamentals\Tubes\monster.csv")
     monster_amt = 0
-    for i in range (len(inventory_array)):
-        if (inventory_array[i][0] == user_id):
-            monster_id = inventory_array[i][1] - 1
-            print((i+1) + '.' + monster_array[monster_id][1], '(Level :', inventory_array[i][2] + ')')
+    for i in range (len(array_monster_inventory)):
+        if (array_monster_inventory[i][0] == user_id):
+            monster_id = array_monster_inventory[i][1] - 1
+            print((i+1) + '.' + array_monster_inventory[monster_id][1], '(Level :', array_monster_inventory[i][2] + ')')
             monster_amt += 1
     print('-------- UPGRADE PRICE LIST --------')
     print('1. Level 1 -> Level 2: 250 OC')
@@ -32,13 +22,13 @@ def laboratory(array, user_id):
     print('3. Level 3 -> Level 4: 800 OC')
     print('4. Level 4 -> Level 5: 1250 OC')
     print('')
-    coin_amt = array[user_id][4]
+    coin_amt = array_user[user_id][4]
     print('Jumlah koin yang dimliki: ', coin_amt) 
     upgrade_id = int(input('Pilih monster yang ingin diupgrade: ')) 
     if (upgrade_id <= 0) or (upgrade_id > monster_amt):
         print('Monster yang ingin diupgrade tidak ditemukan.')
     else:
-        level = inventory_array[upgrade_id - 1][2]
+        level = array_monster_inventory[upgrade_id - 1][2]
         if (level == 5):
             print('Monster yang dipilih sudah memiliki level maksimum.')
         else:
@@ -50,20 +40,20 @@ def laboratory(array, user_id):
                 price = 800
             else: #level + 1 == 5
                 price = 1250
-            print((inventory_array[upgrade_id - 1][1]), 'akan diupdate ke level', (level + 1))
+            print((array_monster_inventory[upgrade_id - 1][1]), 'akan diupdate ke level', (level + 1))
             print('Harga upgrade ini adalah', price)
             confirm = input('Lanjutkan upgrade (Y/N): ')
             while (confirm != 'Y') and (confirm != 'y') and (confirm != 'N') and (confirm != 'n'):
+                print('Input tidak valid!')
                 confirm = input('Lanjutkan upgrade (Y/N): ')
             if (confirm == 'Y') or (confirm == 'y'):
                 if (price > coin_amt):
                     print('Upgrade gagal! Anda tidak memiliki cukup koin.')
                 else:
                     print('Upgrade berhasil!')
-                    inventory_array[upgrade_id - 1][2] += 1 #level ditambah 1
-                    array[user_id][4] -= price #coin_amt dikurang price
-                    print((inventory_array[upgrade_id - 1][1]), 'sekarang ada di level', inventory_array[upgrade_id - 1][2] + '.')
+                    array_monster_inventory[upgrade_id - 1][2] += 1 #level ditambah 1
+                    array_user[user_id][4] -= price #coin_amt dikurang price
+                    print((array_monster_inventory[upgrade_id - 1][1]), 'sekarang ada di level', array_monster_inventory[upgrade_id - 1][2] + '.')
             else: #(confirm == 'N') or (confirm == 'n')
                 print('Upgrade dibatalkan.')
-
-laboratory(array, user_id)
+    return (array_monster_inventory, array_user)
