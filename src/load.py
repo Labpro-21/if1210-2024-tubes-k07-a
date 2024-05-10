0a1346042b6cb309e2875dcd5435b75526950289
Bertha Soliany Frandi <163696570+BerthaSoliany@users.noreply.github.com>
2024-05-10 12:37:59 +0700

    style: separate load function and argparse

--- src/load.py
+++ src/load.py
@@ -1,41 +1,23 @@
-import argparse
-parser = argparse.ArgumentParser(description="load data dari folder yang dipilih")
-parser.add_argument("folder", help="nama folder yang berisi data yang ingin di load")
-args = parser.parse_args()
-folder_path = "./data/"+args.folder
+# FUNGSI load : mengubah data di csv menjadi array
+# Writer: Adrian Sami Pratama
+# Tanggal: 30 April 2024
 
-# mengecek apakah folder ada
-import os
-check_folder = os.path.exists(folder_path)
-if check_folder == True:
-    print("Loading...")
-
-    # fungsi panggil data csv menjadi array
-    def load (filepath: str) -> list:
-        lines = []
-        with open(filepath, 'r') as file:
-            for row in file:
-                lines.append(row) # Mengubah csv menjadi array per baris 
-        array = []
-        for line in lines:
-            row = []
-            kata = ''
-            for elmt in line:
-                if elmt != ';' and elmt != '\n':
-                    kata += elmt
-                elif elmt == ';':
-                    row.append(kata)
-                    kata = ''
-            row.append(kata)
-            array.append(row)
-        return array
-    
-    array_user = load(folder_path+"/user.csv")
-    array_monster = load(folder_path+"/monster.csv")
-    array_monster_inventory = load(folder_path+"/monster_inventory.csv")
-    array_monster_shop = load(folder_path+"/monster_shop.csv")
-    array_item_inventory = load(folder_path+"/item_inventory.csv")
-    array_item_shop = load(folder_path+"/item_shop.csv")
-
-elif check_folder == False:
-    print('Folder "'+args.folder+'" tidak ditemukan')
+# fungsi panggil data csv menjadi array
+def csv_to_array (filepath: str) -> list:
+    lines = []
+    with open(filepath, 'r') as file:
+        for row in file:
+            lines.append(row) # Mengubah csv menjadi array per baris 
+    array = []
+    for line in lines:
+        row = []
+        kata = ''
+        for elmt in line:
+            if elmt != ';' and elmt != '\n':
+                kata += elmt
+            elif elmt == ';':
+                row.append(kata)
+                kata = ''
+        row.append(kata)
+        array.append(row)
+    return array
