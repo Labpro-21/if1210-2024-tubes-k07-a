bf331d7971bd2835c294ee20ac44181dae71a895
Bertha Soliany Frandi <163696570+BerthaSoliany@users.noreply.github.com>
2024-05-10 16:16:15 +0700

    feat: add condition for commands that can be called

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-# load data terlebih dahulu (sebelum program dimulai
+# PROGRAM F14 - Load
+# Writer: Bertha Soliany Frandi
+# Tanggal: 10 Mei 2024
+
 import argparse
 parser = argparse.ArgumentParser(description="load data dari folder yang dipilih")
 parser.add_argument("folder", help="nama folder yang berisi data yang ingin di load")
@@ -9,53 +12,96 @@ folder_path = "./data/"+args.folder
 import os
 check_folder = os.path.exists(folder_path)
 if check_folder == True:
-      print("Loading...")
-      # load data dari folder yang dipilih
-      from load import csv_to_array
-      array_user = csv_to_array(folder_path+"/user.csv")
-      array_monster = csv_to_array(folder_path+"/monster.csv")
-      array_monster_inventory = csv_to_array(folder_path+"/monster_inventory.csv")
-      array_monster_shop = csv_to_array(folder_path+"/monster_shop.csv")
-      array_item_inventory = csv_to_array(folder_path+"/item_inventory.csv")
-      array_item_shop = csv_to_array(folder_path+"/item_shop.csv")
-
-      import time
-      time.sleep(2)
-
-      print("Selamat datang di program OWCA!")
-      time.sleep(1)
-      # meminta perintah berikutnya
-      perintah = input(">>> ")
-      while perintah != "EXIT":
-          if perintah == "REGISTER":
-              import register
-          elif perintah == "LOGIN":
-              import login
-          elif perintah == "LOGOUT":
-              import logout
-          elif perintah == "HELP" or perintah == "MENU":
-              import menu_help
-          elif perintah == "INVENTORY":
-              import inventory
-          elif perintah == "BATTLE":
-              import battle
-          elif perintah == "ARENA":
-              import arena
-          elif perintah == "SHOP":
-              import shop
-          elif perintah == "LABORATORY":
-              import laboratory
-          elif perintah == "SHOP MANAGEMENT": 
-              import shop_management
-          elif perintah == "MONSTER MANAGEMENT":
-              import monster_management
-          elif perintah == "SAVE":
-              import save
-          # meminta input perintah lagi setelah perintah selanjutnya sudah selesai
-          perintah = input(">>> ")
-      if perintah == "EXIT":
-          import exit
+    print("Loading...")
     
+    from load import csv_to_array
+    array_user = csv_to_array(folder_path+"/user.csv")
+    array_monster = csv_to_array(folder_path+"/monster.csv")
+    array_monster_inventory = csv_to_array(folder_path+"/monster_inventory.csv")
+    array_monster_shop = csv_to_array(folder_path+"/monster_shop.csv")
+    array_item_inventory = csv_to_array(folder_path+"/item_inventory.csv")
+    array_item_shop = csv_to_array(folder_path+"/item_shop.csv")
+
+    import time
+    time.sleep(2)
+
+    print("Selamat datang di program OWCA!")
+    time.sleep(1)
+    print()
+
+    # inisialisasi
+    logged_in = False
+    username = ''
+    # meminta perintah berikutnya
+    print('Ketik "HELP" untuk melihat menu')
+    perintah = input(">>> ")
+    while True:
+        if logged_in == False:
+            if perintah == "HELP" or perintah == "MENU":
+                import menu_help
+            elif perintah == "REGISTER":
+                import register
+            elif perintah == "LOGIN":
+                import login
+            elif perintah == "SAVE":
+                import save
+            elif perintah == "EXIT":
+                import exit
+                break
+            else:
+                print("perintah tidak valid!")
+        elif logged_in == True:
+            # mendapatkan role user yang sedang login
+            for i in range(5):
+                if username == array_user[i][1]:
+                    role = array_user[i][3]
+            if role == "admin":
+                if perintah == "HELP" or perintah == "MENU":
+                    import menu_help
+                elif perintah == "REGISTER":
+                    import register
+                elif perintah == "LOGIN":
+                    import login
+                elif perintah == "SHOP MANAGEMENT": 
+                    import shop_management
+                elif perintah == "MONSTER MANAGEMENT":
+                    import monster_management
+                elif perintah == "SAVE":
+                    import save
+                elif perintah == "EXIT":
+                    import exit
+                    break
+                else:
+                    print("Perintah tidak valid!")
+            elif role == "agent":
+                if perintah == "REGISTER":
+                    import register
+                elif perintah == "LOGIN":
+                    import login
+                elif perintah == "LOGOUT":
+                    import logout
+                elif perintah == "HELP" or perintah == "MENU":
+                    import menu_help
+                elif perintah == "INVENTORY":
+                    import inventory
+                elif perintah == "BATTLE":
+                    import battle
+                elif perintah == "ARENA":
+                    import arena
+                elif perintah == "SHOP":
+                    import shop
+                elif perintah == "LABORATORY":
+                    import laboratory
+                elif perintah == "SAVE":
+                    import save
+                elif perintah == "EXIT":
+                    import exit
+                    break
+                else:
+                    print("Perintah tidak valid!")
+        perintah = input(">>> ")
+            
+
 # keluar dari program jika nama folder tidak ditemukan
 elif check_folder == False:
     print('Folder "'+args.folder+'" tidak ditemukan')
