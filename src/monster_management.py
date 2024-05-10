89ea0c95c9dec5e9319db02ea432b482fc73cf38
Naomi Risaka Sitorus <163662184+naomirisaka@users.noreply.github.com>
2024-05-10 15:11:29 +0700

    fix: input validation, adding new element to array_monster

--- src/monster_management.py
+++ src/monster_management.py
@@ -3,19 +3,20 @@
 # Tanggal: 8 Mei 2024
 
 # KAMUS
-# action: integer
+# action, i: integer
 # printing_array, temporary_array: array of array of string
 # max_length_per_column: array of integer
-# i: integer
 # unique_type: boolean
-# monster_type: string
-# monster_def_power, monster_atk_power, monster_hp: integer
-# confirm: string
+# monster_type, monster_def_power, monster_atk_power, monster_hp, confirm : string
 
 # ALGORITMA
-from monster import csv_to_array
+def is_integer(user_input):
+    for char in user_input:
+        if (ord(char) <= ord('0')) or (ord(char) >= ord('9')):
+            return False
+    return True
 
-def monster_management():
+def monster_management(array_monster):
     print('Selamat datang di database monster!')
     print('1. Tampilkan semua monster')
     print('2. Tambahkan monster baru')
@@ -24,11 +25,10 @@ def monster_management():
         print('Aksi yang dipilih tidak ditemukan.')
         action = int(input('Pilih aksi yang ingin dilakukan: '))
     if (action == 1):
-        monster_array = csv_to_array (r"E:\NAOMI\University\Sem 2\Programming Fundamentals\Tubes\monster.csv")
-        printing_array = [['a','b','c','d','e'] for i in range (len(monster_array) + 1)]
+        printing_array = [['a','b','c','d','e'] for i in range (len(array_monster) + 1)]
         printing_array [0] = ['ID', 'Type', 'ATK Power', 'DEF Power', 'HP'] #header line
         for i in range(1, len(printing_array)):
-            printing_array [i] = monster_array [i-1]
+            printing_array [i] = array_monster [i-1]
 
         #print array in form of a structured table
         max_length_per_column = [max(map(len, column)) for column in zip(*printing_array)]
@@ -40,23 +40,23 @@ def monster_management():
         monster_type = input('Masukkan tipe atau nama: ') 
         unique_type = True
         i = 0
-        while (i < len(monster_array)) and (unique_type):
-            if (monster_array [i] == monster_type):
+        while (i < len(array_monster)) and (unique_type):
+            if (array_monster [i] == monster_type):
                 unique_type = False
             i += 1
         while not(unique_type):
             monster_type = input('Masukkan tipe atau nama: ') 
 
         monster_atk_power = int(input('Masukkan ATK Power: '))
-        while not(monster_atk_power.isdigit()):
+        while not(is_integer(monster_atk_power)):
             monster_atk_power = int(input('Masukkan ATK Power: '))
 
         monster_def_power = int(input('Masukkan DEF Power (0-50): ')) 
-        while (not(monster_def_power.isdigit())) or (monster_def_power.isdigit() and ((monster_def_power < 0) or (monster_def_power > 50))):
+        while not(is_integer(monster_def_power)) or (is_integer (monster_def_power) and (int(monster_def_power) < 0 or (int(monster_def_power) > 50))):
             monster_def_power = int(input('Masukkan DEF Power: '))
 
         monster_hp = int(input('Masukkan HP: '))
-        while not(monster_hp.isdigit()):
+        while not(is_integer(monster_hp)):
             monster_hp = int(input('Masukkan HP: '))
         print()
     
@@ -72,16 +72,13 @@ def monster_management():
             confirm = input('Lanjutkan upgrade (Y/N): ')
         if (confirm == 'Y') or (confirm == 'y'):
             #add new monster details
-            temporary_array = [['a','b','c','d','e'] for i in range (len(monster_array) + 1)]
+            temporary_array = [['a','b','c','d','e'] for i in range (len(array_monster) + 1)]
             temporary_array [0] = [
-                (monster_array [len(monster_array) - 1][0] + 1), monster_type, monster_atk_power, monster_def_power, monster_hp] 
+                (array_monster [len(array_monster) - 1][0] + 1), monster_type, int(monster_atk_power), int(monster_def_power), int(monster_hp)] 
             for i in range(1, len(temporary_array)):
-                temporary_array [i] = monster_array [i-1]
-            monster_array = temporary_array
+                temporary_array [i] = array_monster [i-1]
+            array_monster = temporary_array
             print('Monster berhasil ditambahkan ke database!')
         else: #(confirm == 'N') or (confirm == 'n'):
             print('Monster gagal ditambahkan ke database!')
-
-    return monster_array
-
-monster_management()
+    return array_monster
