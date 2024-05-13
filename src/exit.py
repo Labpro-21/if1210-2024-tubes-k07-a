3990b814354aac305c03336056bf4e7c3362a68e
Bertha Soliany Frandi <163696570+BerthaSoliany@users.noreply.github.com>
2024-05-13 08:18:50 +0700

    style: add type safe for exit procedure

--- src/exit.py
+++ src/exit.py
@@ -2,15 +2,17 @@
 # Writer: Bertha Soliany Frandi
 # Tanggal: 6 Mei 2024
 
-def exit():
+# ALGORITMA
+from save import save
+
+def exit(array_user: list, array_monster: list, array_monster_inventory: list, array_item_inventory: list, array_monster_shop: list, array_item_shop: list):
     reassure = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (iya/tidak) ")
     while True:
         if reassure == "iya" or reassure == "IYA" or reassure == "Iya":
-            import save
+            save(array_user, array_monster, array_monster_inventory, array_item_inventory, array_monster_shop, array_item_shop)
             break
         elif reassure == "tidak" or reassure == "TIDAK" or reassure == "Tidak":
             break
         else:
             print("Input tidak valid")
             reassure = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (iya/tidak) ")
-exit()
