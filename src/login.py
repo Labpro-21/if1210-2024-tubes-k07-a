221a83ce8fc15bc91dcee18c103a164ad21ad7b6
Naomi Risaka Sitorus <163662184+naomirisaka@users.noreply.github.com>
2024-05-16 16:23:38 +0700

    fix: role in output message

--- src/login.py
+++ src/login.py
@@ -8,7 +8,7 @@
 # user_id: integer
 
 # ALGORITMA
-def login(logged_in: bool, user_id: int, array_user: list):
+def login(logged_in: bool, user_id: int, array_user: list) -> (bool, str):
     if not logged_in:
         username_found = False
         username = input('Username: ')
@@ -17,7 +17,7 @@ def login(logged_in: bool, user_id: int, array_user: list):
         for i in range(1, len(array_user)):
             if array_user[i][1] == username and array_user[i][2] == password:
                 print('Login berhasil!')
-                print('Selamat datang Agent', username + '!')
+                print('Selamat datang', array_user[i][3], username + '!')
                 print('Ketik "help" untuk melihat list command yang dapat kamu gunakan.')
                 user_id = i
                 return True, user_id  # return login status dan user_id
