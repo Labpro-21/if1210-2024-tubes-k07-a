00fe41f68ddf2667bffc26a564ba54cc93fd8884
Bertha SF <19623107@std.stei.itb.ac.id>
2024-05-18 21:59:30 +0700

    style: change output message

--- src/menu_help.py
+++ src/menu_help.py
@@ -1,6 +1,7 @@
 # PROGRAM F04 - Menu & Help
 # Writer: Michael Alexander Angkawijaya
 # Hari, tanggal: 29 April 2024 (Updated 3 Mei 2024)
+
 # KAMUS 
 # login : boolean
 # role, username : string
@@ -11,18 +12,17 @@ def help(login: bool,role: str,username: str):
     print()
     if(login):  # sudah login
         if(role=="admin"):
-            print("Selamat datang, Admin. Berikut adalah hal-hal yang dapat kamu lakukan:")
+            print("Selamat datang, Admin! Berikut adalah hal-hal yang dapat kamu lakukan:")
             print()
             print("\t1. Logout: Keluar dari akun yang sedang digunakan")
-            print("\t2. Shop-Management: Melakukan manajemen pada SHOP sebagai tempat jual beli peralatan Agent")
-            print("\t3. Monster-Management: Melakukan manajemen pada database monster")
+            print("\t2. Shop Management: Melakukan manajemen pada SHOP sebagai tempat jual beli peralatan Agent")
+            print("\t3. Monster Management: Melakukan manajemen pada database monster")
             print("\t4. Save: Menyimpan data ke dalam folder")
             print("\t5. Exit: Keluar dari program")
         else: #role == "agent"
-            print(f"Halo Agent {username}. Kamu memanggil command HELP. Kamu memilih jalan yang benar, semoga kamu tidak sesat kemudian. Berikut adalah hal-hal yang dapat kamu lakukan sekarang:")
+            print(f"Halo Agent {username}! Kamu memanggil command HELP. Kamu memilih jalan yang benar, semoga kamu tidak sesat kemudian. Berikut adalah hal-hal yang dapat kamu lakukan sekarang:")
             print()
             print("\t1. Logout: Keluar dari akun yang sedang digunakan")
-            # print("\t2. Monster: Melihat owca-dex yang dimiliki oleh Agent")
             print("\t2. Inventory: Melihat detail monster, potion, dan OC yang dimiliki oleh Agent")
             print("\t3. Battle: Bertarung 1v1 melawan monster")
             print("\t4. Arena: Melatih monster yang dimiliki oleh Agent")
