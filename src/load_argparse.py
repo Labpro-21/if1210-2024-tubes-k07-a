1d2ef9c36d6c4bc9a451ad22d66043979e4a771d
Bertha Soliany Frandi <163696570+BerthaSoliany@users.noreply.github.com>
2024-05-18 10:20:40 +0700

    fix: customizing error message and rename file

---
+++ src/load_argparse.py
@@ -0,0 +1,35 @@
+# PROGRAM F14 - Load
+# Writer: Bertha Soliany Frandi
+# Tanggal: 10 Mei 2024
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description="load data dari folder yang dipilih")
+parser.add_argument("folder", nargs='?', help="nama folder yang berisi data yang ingin di load")
+args = parser.parse_args()
+
+# mengeluarkan error message
+if not args.folder:
+    print("Tidak ada nama folder yang diberikan!")
+    print("Usage : python main.py <nama_folder>")
+    sys.exit()
+else:
+    folder_path = "./data/"+args.folder
+    # mengecek apakah folder ada
+    check_folder = os.path.exists(folder_path)
+    if check_folder == True:
+        print("Loading...")
+        
+        from load import csv_to_array
+        array_user = csv_to_array(folder_path+"/user.csv")
+        array_monster = csv_to_array(folder_path+"/monster.csv")
+        array_monster_inventory = csv_to_array(folder_path+"/monster_inventory.csv")
+        array_monster_shop = csv_to_array(folder_path+"/monster_shop.csv")
+        array_item_inventory = csv_to_array(folder_path+"/item_inventory.csv")
+        array_item_shop = csv_to_array(folder_path+"/item_shop.csv")
+        
+        print("Selamat datang di program OWCA!")
+    
+    elif check_folder == False:
+        print('Folder "'+args.folder+'" tidak ditemukan')
