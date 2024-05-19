2d591726f78fc488482d44f3f9ad1361d2fb708f
Michael Alexander Angkawijaya <19623157@std.stei.itb.ac.id>
2024-05-19 23:11:59 +0700

    merge conflicts

--- src/logout.py
+++ src/logout.py
@@ -4,17 +4,20 @@
 # Note: logout dilakukan saat login sudah dilaksanakan
 
 # Algoritma
-def check_login():
-    return False
-
-def logout():
-    if check_login():
-        log_out = input("Apakah anda ingin keluar dari permainan? (iya / tidak): ")
-        if log_out() == "iya":
-            print("User berhasil melakukan logout.")
-        else:
-            print("Logout dibatalkan.")
+def logout(logged_in: bool) -> bool:
+    if logged_in:
+        log_out = input("Apakah anda ingin keluar dari permainan? (Y/N): ").upper()
+        while True:
+            if log_out == "Y":
+                print("User berhasil melakukan logout!")
+                return False # sudah tidak login
+            elif log_out == "N":
+                print("Logout dibatalkan! Silahkan melanjutkan misi kembali.")
+                return True # masih login
+            else:
+                print("Input tidak valid. Masukkan antara Y/N.")
+                log_out = input("Apakah anda ingin keluar dari permainan? (Y/N): ").upper()
     else:
-        print("User belum login.")
+        print("User belum login!") 
+        return False # memang belum login
 
-logout()
