4ff6ac452afcf6d26a6b52b96c956e65a7943cbb
Bertha SF <19623107@std.stei.itb.ac.id>
2024-05-20 05:27:42 +0700

    fix: fix logged_in validation and choosing monster bug

--- src/register.py
+++ src/register.py
@@ -14,14 +14,16 @@ def register(logged_in: bool, array_user: list, array_monster: list, array_monst
     # username, password: string
 
     # ALGORITMA
+    print("Selamat datang di register!\n")
     while True:
         username = input("Masukkan username: ")
         password = input("Masukkan password: ")
 
+        print()
         # syarat dimana user tidak boleh dalam keadaan sedang login
         if logged_in:
             print("Register gagal")
-            print(f"Anda masih login dengan username {username}. Silahkan logout terlebih dahulu sebelum melakukan register.")
+            print(f"Anda masih login dengan username {username}. Silahkan logout terlebih dahulu sebelum melakukan register.\n")
         else:
             return cekValid(username, password, array_user, array_monster, array_monster_inventory)
     
@@ -64,72 +66,73 @@ def cekValid(username: str, password:str, array_user: list, array_monster: list,
 
     # syarat username yang tidak boleh sama seperti agent lain yang sudah ada
     ada = False
-    while True:
-        for i in range(len(array_user)):
+    for i in range(len(array_user)):
+        while True:
             if array_user[i][1] == username:
                 print(f"Username sudah terpakai, silahkan gunakan username lain.\n")
                 ada = True
                 username = input("Masukkan username: ")
                 password = input("Masukkan password: ")
                 break
-        if not(ada): 
-            break
+            else:
+                ada = False
+                break
+    if ada == False: 
+        # melakukan pengecekan terhadap password
+        cekPassword(password)
 
-    # melakukan pengecekan terhadap password
-    cekPassword(password)
+        # menyimpan data user baru
+        data_user = [0 for i in range(5)]
+        maks = -1
+        for i in range(1, len(array_user)):
+            if int(array_user[i][0]) < maks:
+                    maks = int(array_user[i][0])
+        data_user = [str(int(array_user[maks][0]) + 1), username, password, 'agent', '0']
+        array_user.append(data_user) # menyimpan user baru ke baris paling bawah di array_user
+        print()
 
-    # menyimpan data user baru
-    data_user = [0 for i in range(5)]
-    maks = -1
-    for i in range(1, len(array_user)):
-        if int(array_user[i][0]) < maks:
-                maks = int(array_user[i][0])
-    data_user = [str(int(array_user[maks][0]) + 1), username, password, 'agent', '0']
-    array_user.append(data_user) # menyimpan user baru ke baris paling bawah di array_user
-    print()
-
-    # untuk memilih monster awal
-    print("Silahkan pilih salah satu monster sebagai monster awalmu.")
-    batas_awal = 9999
-    batas_akhir = -1
-    for i in range(len(array_monster)):
-        if i != 0 and i <= 3:
-            print(f"{i}"+".",end=" ")
-            print(array_monster[i][1])
-            if i <= batas_awal:
-                batas_awal = i
-            if i >= batas_akhir:
-                batas_akhir = i 
-    print()
-    print("Masukkan angka dari monster yang dipilih.")
-    monster_pilihan = input("Monster pilihanmu: ")
-    while True:
-        if(is_integer(monster_pilihan)):
-            monster_pilihan = int(monster_pilihan)
-            break
-        elif monster_pilihan == '':
-            print("Monster harus dipilih dan tidak boleh kosong. Masukkan angka untuk monster yang ingin pilih.\n")
-        else:
-            print("Input tidak valid. Coba pilih ulang.\n")
-        monster_pilihan = input("Monster pilihanmu: ")
-
-    while monster_pilihan <= 1 or monster_pilihan >= 3:
-        print("input tidak valid. Coba pilih ulang.\n")
+        # untuk memilih monster awal
+        print("Silahkan pilih salah satu monster sebagai monster awalmu.")
+        batas_awal = 9999
+        batas_akhir = -1
+        for i in range(len(array_monster)):
+            if i != 0 and i <= 3:
+                print(f"{i}"+".",end=" ")
+                print(array_monster[i][1])
+                if i <= batas_awal:
+                    batas_awal = i
+                if i >= batas_akhir:
+                    batas_akhir = i 
+        print()
+        print("Masukkan angka dari monster yang dipilih.")
         monster_pilihan = input("Monster pilihanmu: ")
         while True:
             if(is_integer(monster_pilihan)):
                 monster_pilihan = int(monster_pilihan)
                 break
-            print("input tidak valid. Coba pilih ulang.\n")
+            elif monster_pilihan == '':
+                print("Monster harus dipilih dan tidak boleh kosong. Masukkan angka untuk monster yang ingin pilih.\n")
+            else:
+                print("Input tidak valid. Coba pilih ulang.\n")
             monster_pilihan = input("Monster pilihanmu: ")
 
-    nama_monster_pilihan = array_monster[monster_pilihan][1]
-    print()
-    print(f"Selamat datang Agent {username}. Mari kita mengalahkan Dr. Asep Spakbor dengan {nama_monster_pilihan}!")
-    # menyimpan monster pilihan ke array csv monster_inventory
-    data_monster_inventory = [data_user[0], str(monster_pilihan), '1']
-    array_monster_inventory.append(data_monster_inventory)
-    return array_user,array_monster_inventory
+        while monster_pilihan <= 1 and monster_pilihan >= 3:
+            print("input tidak valid. Coba pilih ulang.\n")
+            monster_pilihan = input("Monster pilihanmu: ")
+            while True:
+                if(is_integer(monster_pilihan)):
+                    monster_pilihan = int(monster_pilihan)
+                    break
+                print("input tidak valid. Coba pilih ulang.\n")
+                monster_pilihan = input("Monster pilihanmu: ")
+
+        nama_monster_pilihan = array_monster[monster_pilihan][1]
+        print()
+        print(f"Selamat datang Agent {username}. Mari kita mengalahkan Dr. Asep Spakbor dengan {nama_monster_pilihan}!")
+        # menyimpan monster pilihan ke array csv monster_inventory
+        data_monster_inventory = [data_user[0], str(monster_pilihan), '1']
+        array_monster_inventory.append(data_monster_inventory)
+        return array_user,array_monster_inventory
 
 def cekPassword(password: str) -> str:
     # KAMUS LOKAL
